bdf4e8b2ed8490ced06a059c782695bcf6516365
Ashumr <igotwebmail@gmail.com>
2022-07-08 15:17:59 -0400

    working candy api

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const PORT = 5500
+
+const candies = {
+    'snickers':{
+       'type': 'chocolate',
+       'flavor': 'caramel peanut filling'
+    },
+    'fererro rocher':{
+        'type':'chocolate',
+        'flavor':'hazelnut'
+    },
+
+    'sour patch kids':{
+        'type':'hard candy',
+        'flavor':'watermelon'
+    },
+
+    'unknown':{
+        'age':0,
+        'type':'unknown',
+        'flavor':'unknown'
+    }
+}
+
+
+app.get('/', (request, response)=> {
+    response.sendFile(__dirname + '/index.html')
+
+})
+
+app.get('/api/:name',(request,response)=>{
+    const candyName = request.params.name.toLowerCase()
+    if(candies[candyName]){
+    response.json(candies[candyName])
+    }
+    else{
+        response.json(candies['unknown'])
+    }
+})
+
+
+app.listen(PORT, ()=> {
+    console.log(`The server is now running on port ${PORT}! Betta 
+    Go Catch It!`)
+})
